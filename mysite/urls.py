3250949896aeb52719e49d12ba6816c64e230099
Mukesh Kumar <krmukesh94@gmail.com>
2020-05-17 02:15:46 +0530

    jitsimeet config for meeting

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^teacher$', views.teacher, name='teacher'),
+    url(r'^student$', views.student, name='student'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
+
+def teacher(request):
+    context = {
+        'room_name': 'saturday12345kumar',
+        'name': 'Mukesh kumar',
+        'subject': 'physics',
+    }
+    return render(request, 'teacher.html', context)
+
+def student(request):
+    context = {
+        'room_name': 'saturday12345kumar',
+        'name': 'Mukesh kumar',
+        'subject': 'physics',
+    }
+    return render(request, 'student.html', context)
